4639666069f216d3afe4f0acdaa3efc597a5a6c5
echo61546@gmail.com <echo61546@gmail.com>
2019-05-17 20:26:31 +0800

    跨域

--- app/Http/Controllers/Api/Controller.php
+++ app/Http/Controllers/Api/Controller.php
@@ -4,9 +4,14 @@ namespace App\Http\Controllers\Api;
 
 use Dingo\Api\Routing\Helpers;
 use App\Http\Controllers\Controller as BaseController;
-header('Access-Control-Allow-Origin:*');
 
 class Controller extends BaseController
 {
     use Helpers;
+
+//    public function __construct()
+//    {
+//        header('Access-Control-Allow-Origin:*');
+//        header('Access-Control-Allow-Methods:GET, POST, PATCH, PUT, OPTIONS');
+//    }
 }
